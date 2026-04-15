76ccdf099e4027535e29eb5eb43f1b14d9ebd4ec
nano <nano@example.com>
2026-04-15 21:37:52 +0900

    ripgrep install

--- vim-plug-ver/hoge.c
+++ vim-plug-ver/hoge.c
@@ -1,9 +1,8 @@
-#include <stdio.h>
+:set grepprg=rg\ --vimgrep\ --smart-case#include <stdio.h>
 #include <stdlib.h>
 
 void main(void){
   printf("");
-
 
   printf("");
 }
---
+++ vim-plug-ver/hoge.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+printf("aefa");
+
+printf("%d",30);
