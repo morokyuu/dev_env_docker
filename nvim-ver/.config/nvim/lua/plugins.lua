dcca66b8bc615e64b8d5d0be3ed78610ff9441bc
nano <nano@example.com>
2026-06-28 15:18:23 +0900

    add: complete neovim-cmp

--- nvim-ver/.config/nvim/lua/plugins.lua
+++ nvim-ver/.config/nvim/lua/plugins.lua
@@ -33,8 +33,34 @@ require("lazy").setup({
     config = function()
       require("lualine").setup()
     end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+    },
+    config = function()
+      local cmp = require("cmp")
+
+      cmp.setup({
+--        mapping = cmp.mapping.preset.insert({
+        mapping = {
+          ["<c-n>"] = cmp.mapping.select_next_item(),
+          ["<c-p>"] = cmp.mapping.select_prev_item(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        },
+
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "buffer" },
+          { name = "path" },
+        },
+      })
+    end,
   }
 })
-
 
 
