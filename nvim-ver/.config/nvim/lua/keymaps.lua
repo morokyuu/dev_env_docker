c5352d2b8ab66cb48dc31cc487cbe171dc1943e9
nano <nano@example.com>
2026-06-28 10:49:12 +0900

    add: nvim .config folder

---
+++ nvim-ver/.config/nvim/lua/keymaps.lua
@@ -0,0 +1,18 @@
+
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(event)
+		local opts = { buffer = event.buf }
+
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+		vim.keymap.set("n", "gr", vim.lsp.buf.references)
+		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
+		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.buf.code_action)
+		vim.keymap.set("n", "gl", vim.diagnostic.open_float, opts)
+		vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+		vim.keymap.set("n", "d]", vim.diagnostic.goto_next, opts)
+	end,
+})
+
---
+++ nvim-ver/.config/nvim/lua/lsp.lua
@@ -0,0 +1,7 @@
+vim.lsp.config("lua_ls", {})
+vim.lsp.enable("lua_ls")
+
+--vim.lsp.config("pyright", {})
+--vim.lsp.enable("pyright")
+
+
